ac4a8f45769ae0dd9063411108d0b6fa7f362cfc
shampoojr <shampoojr@outlook.com>
2025-02-26 12:53:29 +0100

    Removed some packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,9 @@
 { pkgs, config, ... }:
 {
+
+  imports = [
+    ./programs.nix
+  ];
   home.packages = [
 pkgs.dconf
 config.programs.spicetify.spicedSpotify
---
+++ packages/programs.nix
@@ -0,0 +1,16 @@
+{config, pkgs,...}:{
+  programs = {
+    steam = {
+      enable = true;
+    };
+    java = {
+      enable = true;
+    };
+    thunar = {
+      enable = true;
+    };
+    direnv={
+      enable = true;
+    };
+  };
+}
