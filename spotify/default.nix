54c4a2eb713fb5918ed384a5541bcb6f9e7895e0
shampoojr <shampoojr@outlook.com>
2025-02-26 11:13:41 +0100

    Spotify and Spicetify

--- spotify/default.nix
+++ spotify/default.nix
@@ -6,22 +6,37 @@
   spicePkgs,
   ...
 }:
-let
-
-in
 {
-
   programs.spicetify.dontInstall = true;
 
   programs.spicetify = {
     enable = true;
-    #config options
     enabledExtensions = with spicePkgs.extensions; [
       hidePodcasts
-    #  shuffle # shuffle+ (special characters are sanitized out of extension names)
+      beautifulLyrics
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
     ];
-         theme = spicePkgs.themes.ziro;
-     #colorScheme = "ziro";
+    enabledCustomApps = with spicePkgs.apps; [
+      marketplace
+    ];
+    enabledSnippets = with spicePkgs.snippets; [
+      fixListenTogetherButton
+      removeDuplicatedFullscreenButton
+      declutterNowPlayingBar
+      queueTopSidePanel
+      fixLikedIcon
+      pointer
+      hideLyricsButton
+      hideNowPlayingViewButton
+      hideFullScreenButton
+      hideMiniPlayerButton
+      hideWhatsNewButton
+      hideFriendActivityButton
+      hideMadeForYou
+
+    ];
+    theme = spicePkgs.themes.ziro;
+    #colorScheme = "";
   };
 
 }
