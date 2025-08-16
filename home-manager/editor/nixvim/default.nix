e6863c68dd83dbc3f4e52fb134b5601f195b39d6
shampoojr <shampoojr@outlook.com>
2025-08-16 21:35:19 +0200

    nixvim

--- home-manager/editor/nixvim/default.nix
+++ home-manager/editor/nixvim/default.nix
@@ -2,31 +2,14 @@
 {
   imports = [
     inputs.nixvim.homeModules.nixvim
-    ./plugins/highlights.nix
-    ./plugins/lazy.nix
-    ./plugins/lint.nix
-    ./plugins/lualine.nix
-    ./plugins/mini.nix
-    ./plugins/neocord.nix
-    ./plugins/neotree.nix
-    ./plugins/nix.nix
-    ./plugins/nui.nix
-    ./plugins/snack.nix
-    ./plugins/telescope.nix
-    ./plugins/treesitter.nix
-    ./plugins/visual-whitespaces.nix
-    ./plugins/web-devicons.nix
+    ./plugins
+    ./keymaps.nix
+    ./opts.nix
   ];
   programs = {
     nixvim = {
       enable = true;
-      keymaps = [
-      ];
-      opts = {
-      };
       colorschemes.catppuccin.enable = true;
-      plugins = {
-      };
     };
   };
 }
---
+++ home-manager/editor/nixvim/plugins/default.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ./highlightcolor.nix
+    ./lazy.nix
+    ./lint.nix
+    ./lualine.nix
+    ./mini.nix
+    ./neocord.nix
+    ./neotree.nix
+    ./nix.nix
+    ./nui.nix
+    ./snack.nix
+    ./telescope.nix
+    ./treesitter.nix
+    ./visual-whitespace.nix
+    ./web-devicons.nix
+  ]
+}
