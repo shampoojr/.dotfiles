b240591cef700f6b03278ec904e288b9a6ae199a
shampoojr <shampoojr@outlook.com>
2025-08-16 16:20:49 +0200

    secureboot

---
+++ hosts/hardware/secureboot/default.nix
@@ -0,0 +1,15 @@
+{ lanzaboote, pkgs, lib, ... }:
+{
+  imports = [ lanzaboote.nixosModules.lanzaboote ];
+
+  environment.systemPackages = [
+    pkgs.sbctl
+  ];
+
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+}
