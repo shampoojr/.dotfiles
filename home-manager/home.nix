4a7f6cf685a07b85167b42770d712c895e094ba4
shampoojr <shampoojr@outlook.com>
2025-08-21 00:05:24 +0200

    Changed stateVersion

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -10,7 +10,7 @@
   home = {
     username = "${username}";
     homeDirectory = "/home/shampoojr";
-    stateVersion = "25.05"; # Please read the comment before changing.
+    stateVersion = "25.11"; # Please read the comment before changing.
     file = { };
     sessionVariables = {
       XDG_SCREENSHOTS_DIR = "$HOME/Pictures/Screenshots";
