3bf14d1ffb71aa2657b50cd6eab5b316b8bc3b36
shampoojr <shampoojr@outlook.com>
2025-09-03 22:39:06 +0200

    Added spaces and deleted an application think it was freecad

--- home-manager/packages/default.nix
+++ home-manager/packages/default.nix
@@ -11,7 +11,6 @@
       btop
       dconf
       eww
-      freecad
       gh
       hyprpaper
       hyprpicker
--- home-manager/packages/programs.nix
+++ home-manager/packages/programs.nix
@@ -4,10 +4,12 @@
     direnv = {
       enable = true;
     };
+    
     java = {
       enable = true;
     };
   };
+
   services = {
     mpris-proxy = {
       enable = true;
