6550195232cc1a345291062b381daccde30c448b
shampoojr <shampoojr@outlook.com>
2025-09-16 18:12:19 +0200

    Added Hyprexpo

---
+++ home/hyprland/hypr/configs/hyprexpo.nix
@@ -0,0 +1,13 @@
+{
+  wayland.windowManager.hyprland = {
+    plugin = {
+      hyprexpo = {
+        columns = 1;
+        gap_size = 5;
+        bg_col = "#f1f1f1";
+        workspace_method = center current;
+        gesture_distance = 300;
+      };
+    };
+  };
+}
