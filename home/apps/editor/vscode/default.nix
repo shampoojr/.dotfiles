8e433b6e8244a767419992ebd6ebca8e9312e6f8
shampoojr <shampoojr@outlook.com>
2025-12-21 21:47:22 +0100

    VScode settings

--- home/apps/editor/vscode/default.nix
+++ home/apps/editor/vscode/default.nix
@@ -19,6 +19,24 @@
           jnoortheen.nix-ide
           codezombiech.gitignore
         ];
+
+        userSettings = {
+          "editor.defaultFormatter" = "theqtcompany.qt-qml";
+          "editor.fontSize" = 18;
+          "editor.fontFamily" = "FiraCode Nerd Font";
+          "editor.formatOnSave" = true;
+          "editor.minimap.renderCharacters" = false;
+          "editor.minimap.scale" = 2;
+          "[nix]" = {
+            "editor.defaultFormatter" = "kamadorueda.alejandra";
+            "editor.formatOnPaste" = true;
+            "editor.formatOnSave" = true;
+            "editor.formatOnType" = false;
+          };
+          "alejandra.program" = "alejandra";
+          "workbench.colorTheme" = "Catppuccin Mocha";
+          "vscord.status.state.text.editing" = "Working on {file_name}{file_extension}:{current_line}:{current_column}";
+        };
       };
     };
   };
