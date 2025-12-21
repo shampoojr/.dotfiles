8163c0bc25d5cd9d3d1bda6a5a0f4576dfff5356
shampoojr <shampoojr@outlook.com>
2025-12-21 21:46:04 +0100

    stylix

---
+++ home/hyprland/stylix/default.nix
@@ -0,0 +1,12 @@
+{...}: {
+  stylix = {
+    enable = true;
+    image = [
+      "~/Pictures/wallpaper/os/nixos/nix-black-4k.png"
+    ];
+    targets = {
+      kitty = {
+      };
+    };
+  };
+}
